b656a37faf40087db46c9ec2d1f1958e51179b37
Aline Maysa Momm <alinemaysa@gmail.com>
2025-08-11 20:07:10 -0300

    Add contact form and result display to Flask application

---
+++ Semana_3/meu_app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# render_template = levar para outra página
+# request = pegar dados do usuário
+#@app.route('/sobre')  # rota para a página "sobre"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nome = request.form.get("nome")
+        email = request.form.get("email")
+        mensagem = request.form.get("mensagem")
+        return render_template('resultado.html', nome=nome, email=email, mensagem=mensagem)
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':  # só roda se for o arquivo principal
+    app.run(debug=True)     # debug=True = reinicia o servidor automaticamente quando salva o arquivo
+    
+    
